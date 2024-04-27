59b95809737fec5cf74d3e88fed99c990cd0f468
danielaPalma04 <160284466+danielaPalma04@users.noreply.github.com>
2024-04-27 11:03:47 -0600

    Add files via upload

---
+++ segundoParcial/calculadora/calculadora/Calculadora.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+
+    class Calculadora
+    {
+        public double Sumar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Restar(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiplicar(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Dividir(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return num1 / num2;
+        }
+
+        class CalculadoraCientifica : Calculadora
+        {
+            public double RaizCuadrada(double num)
+            {
+                if (num < 0)
+
+                    return Math.Sqrt(num);
+            }
+
+            public double Potencia(double num, double exponente)
+            {
+                return Math.Pow(num, exponente);
+            }
+
+            public double Seno(double angulo)
+            {
+                return Math.Sin(angulo);
+            }
+
+            public double Coseno(double angulo)
+            {
+                return Math.Cos(angulo);
+            }
+
+            public double Tangente(double angulo)
+            {
+                if (Math.Cos(angulo) == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+
+                return Math.Tan(angulo);
+            }
+        }
+    }
+}
